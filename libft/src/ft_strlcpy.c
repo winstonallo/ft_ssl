abe3856e6b8cd917e2b2ccc1d0801409393fbf98
winstonallo <arthurbiedchar@gmail.com>
2024-11-30 18:16:03 +0100

    fix: issue related to reallocation

--- libft/src/ft_strlcpy.c
+++ libft/src/ft_strlcpy.c
@@ -17,15 +17,19 @@ ft_strlcpy(char *dst, const char *src, size_t size) {
     size_t i;
 
     i = 0;
-    if (size == 0)
+    if (size == 0) {
+
         return (ft_strlen(src));
+	}
     while (i < size - 1 && src[i] != '\0') {
         dst[i] = src[i];
         i++;
     }
-    if (size > 0)
+    if (size > 0) {
         dst[i] = '\0';
-    // while (src[i] != '\0')
-    // 	i++;
+    }
+    while (src[i] != '\0') {
+        i++;
+    }
     return (i);
 }
