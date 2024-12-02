58340a8f7247e72592509e67587e2c77c21aa2d7
winstonallo <arthurbiedchar@gmail.com>
2024-12-02 09:12:15 +0100

    fix: endianness in padding u64 and output buffer

--- src/sha256.c
+++ src/sha256.c
@@ -9,37 +9,36 @@
 
 // First 32 bits of the fractional parts of the square roots of the first eight
 // prime numbers.
-#define DFLT_A 0x6a09e667
-#define DFLT_B 0xbb67ae85
-#define DFLT_C 0x3c6ef372
-#define DFLT_D 0xa54ff53a
-#define DFLT_E 0x510e527f
-#define DFLT_F 0x9b05688c
-#define DFLT_G 0x1f83d9ab
-#define DFLT_H 0x5be0cd19
+#define DFLT_A 0x6a09e667UL
+#define DFLT_B 0xbb67ae85UL
+#define DFLT_C 0x3c6ef372UL
+#define DFLT_D 0xa54ff53aUL
+#define DFLT_E 0x510e527fUL
+#define DFLT_F 0x9b05688cUL
+#define DFLT_G 0x1f83d9abUL
+#define DFLT_H 0x5be0cd19UL
 
 // First 32 bits of the fractional parts of the cube roots of the first 64
 // prime numbers.
 static const u_int32_t K[] = {
-    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-   0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-   0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-   0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-   0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-   0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-   0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-   0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-};
+    0x428a2f98UL, 0x71374491UL, 0xb5c0fbcfUL, 0xe9b5dba5UL, 0x3956c25bUL, 0x59f111f1UL, 0x923f82a4UL, 0xab1c5ed5UL,
+    0xd807aa98UL, 0x12835b01UL, 0x243185beUL, 0x550c7dc3UL, 0x72be5d74UL, 0x80deb1feUL, 0x9bdc06a7UL, 0xc19bf174UL,
+    0xe49b69c1UL, 0xefbe4786UL, 0x0fc19dc6UL, 0x240ca1ccUL, 0x2de92c6fUL, 0x4a7484aaUL, 0x5cb0a9dcUL, 0x76f988daUL,
+    0x983e5152UL, 0xa831c66dUL, 0xb00327c8UL, 0xbf597fc7UL, 0xc6e00bf3UL, 0xd5a79147UL, 0x06ca6351UL, 0x14292967UL,
+    0x27b70a85UL, 0x2e1b2138UL, 0x4d2c6dfcUL, 0x53380d13UL, 0x650a7354UL, 0x766a0abbUL, 0x81c2c92eUL, 0x92722c85UL,
+    0xa2bfe8a1UL, 0xa81a664bUL, 0xc24b8b70UL, 0xc76c51a3UL, 0xd192e819UL, 0xd6990624UL, 0xf40e3585UL, 0x106aa070UL,
+    0x19a4c116UL, 0x1e376c08UL, 0x2748774cUL, 0x34b0bcb5UL, 0x391c0cb3UL, 0x4ed8aa4aUL, 0x5b9cca4fUL, 0x682e6ff3UL,
+    0x748f82eeUL, 0x78a5636fUL, 0x84c87814UL, 0x8cc70208UL, 0x90befffaUL, 0xa4506cebUL, 0xbef9a3f7UL, 0xc67178f2};
 
 typedef struct Words {
-    uint32_t A;
-    uint32_t B;
-    uint32_t C;
-    uint32_t D;
-    uint32_t E;
-    uint32_t F;
-    uint32_t G;
-    uint32_t H;
+    uint32_t a;
+    uint32_t b;
+    uint32_t c;
+    uint32_t d;
+    uint32_t e;
+    uint32_t f;
+    uint32_t g;
+    uint32_t h;
 } Words;
 
 static uint32_t
@@ -105,10 +104,7 @@ sha256_pad(File *msg) {
     }
 
     buf.bytes[msg->content_size] = (char)0x80;
-    *(uint64_t *)(&buf.bytes[new_size - 8]) = __builtin_bswap32(msg->content_size * 8);
-
-    
-
+    *(uint64_t *)(&buf.bytes[new_size - 8]) = (uint64_t)__builtin_bswap64(msg->content_size * 8);
     buf.len = new_size;
 
     return buf;
@@ -116,56 +112,56 @@ sha256_pad(File *msg) {
 
 static void
 store_to_buf(char *buf, Words words) {
-    uint32_t A = words.A;
-    uint32_t B = words.B;
-    uint32_t C = words.C;
-    uint32_t D = words.D;
-    uint32_t E = words.E;
-    uint32_t F = words.G;
-    uint32_t G = words.H;
-    uint32_t H = words.H;
+    uint32_t a = words.a;
+    uint32_t b = words.b;
+    uint32_t c = words.c;
+    uint32_t d = words.d;
+    uint32_t e = words.e;
+    uint32_t f = words.f;
+    uint32_t g = words.g;
+    uint32_t h = words.h;
 
     int idx = 0;
 
-    byte_to_hex(A & 0xFF, buf, &idx);
-    byte_to_hex((A >> 8) & 0xFF, buf, &idx);
-    byte_to_hex((A >> 16) & 0xFF, buf, &idx);
-    byte_to_hex((A >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((a >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((a >> 16) & 0xFF, buf, &idx);
+    byte_to_hex((a >> 8) & 0xFF, buf, &idx);
+    byte_to_hex(a & 0xFF, buf, &idx);
 
-    byte_to_hex(B & 0xFF, buf, &idx);
-    byte_to_hex((B >> 8) & 0xFF, buf, &idx);
-    byte_to_hex((B >> 16) & 0xFF, buf, &idx);
-    byte_to_hex((B >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((b >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((b >> 16) & 0xFF, buf, &idx);
+    byte_to_hex((b >> 8) & 0xFF, buf, &idx);
+    byte_to_hex(b & 0xFF, buf, &idx);
 
-    byte_to_hex(C & 0xFF, buf, &idx);
-    byte_to_hex((C >> 8) & 0xFF, buf, &idx);
-    byte_to_hex((C >> 16) & 0xFF, buf, &idx);
-    byte_to_hex((C >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((c >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((c >> 16) & 0xFF, buf, &idx);
+    byte_to_hex((c >> 8) & 0xFF, buf, &idx);
+    byte_to_hex(c & 0xFF, buf, &idx);
 
-    byte_to_hex(D & 0xFF, buf, &idx);
-    byte_to_hex((D >> 8) & 0xFF, buf, &idx);
-    byte_to_hex((D >> 16) & 0xFF, buf, &idx);
-    byte_to_hex((D >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((d >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((d >> 16) & 0xFF, buf, &idx);
+    byte_to_hex((d >> 8) & 0xFF, buf, &idx);
+    byte_to_hex(d & 0xFF, buf, &idx);
 
-    byte_to_hex(E & 0xFF, buf, &idx);
-    byte_to_hex((E >> 8) & 0xFF, buf, &idx);
-    byte_to_hex((E >> 16) & 0xFF, buf, &idx);
-    byte_to_hex((E >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((e >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((e >> 16) & 0xFF, buf, &idx);
+    byte_to_hex((e >> 8) & 0xFF, buf, &idx);
+    byte_to_hex(e & 0xFF, buf, &idx);
 
-    byte_to_hex(F & 0xFF, buf, &idx);
-    byte_to_hex((F >> 8) & 0xFF, buf, &idx);
-    byte_to_hex((F >> 16) & 0xFF, buf, &idx);
-    byte_to_hex((F >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((f >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((f >> 16) & 0xFF, buf, &idx);
+    byte_to_hex((f >> 8) & 0xFF, buf, &idx);
+    byte_to_hex(f & 0xFF, buf, &idx);
 
-    byte_to_hex(G & 0xFF, buf, &idx);
-    byte_to_hex((G >> 8) & 0xFF, buf, &idx);
-    byte_to_hex((G >> 16) & 0xFF, buf, &idx);
-    byte_to_hex((G >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((g >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((g >> 16) & 0xFF, buf, &idx);
+    byte_to_hex((g >> 8) & 0xFF, buf, &idx);
+    byte_to_hex(g & 0xFF, buf, &idx);
 
-    byte_to_hex(H & 0xFF, buf, &idx);
-    byte_to_hex((H >> 8) & 0xFF, buf, &idx);
-    byte_to_hex((H >> 16) & 0xFF, buf, &idx);
-    byte_to_hex((H >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((h >> 24) & 0xFF, buf, &idx);
+    byte_to_hex((h >> 16) & 0xFF, buf, &idx);
+    byte_to_hex((h >> 8) & 0xFF, buf, &idx);
+    byte_to_hex(h & 0xFF, buf, &idx);
 
     buf[idx] = '\0';
 }
@@ -181,48 +177,47 @@ sha256_hash(File *msg, Words *words) {
 
     for (uint8_t *chunk = buf.bytes; (size_t)chunk - (size_t)buf.bytes < buf.len; chunk += SHA256_BLOCK_SIZE) {
 
-        uint32_t *block = (void *)chunk;
+        uint32_t W[64];
 
-        uint32_t sched[64];
-
-        // First 16 words of the schedule are just copied from the chunk directly.
-        ft_memcpy(sched, block, SHA256_BLOCK_SIZE);
-
-        for (size_t step = 16; step < 64; ++step) {
-            sched[step] = sig1(sched[step - 2]) + sched[step - 7] + sig0(sched[step - 15]) + sched[step - 16];
+        for (size_t t = 0; t < 16; ++t) {
+            W[t] = __builtin_bswap32(((uint32_t *)chunk)[t]);
         }
 
-        uint32_t A = words->A;
-        uint32_t B = words->B;
-        uint32_t C = words->C;
-        uint32_t D = words->D;
-        uint32_t E = words->E;
-        uint32_t F = words->F;
-        uint32_t G = words->G;
-        uint32_t H = words->H;
-
-        for (size_t step = 0; step < 64; ++step) {
-            uint32_t t1 = H + Sig1(E) + Ch(E, F, G) + H + K[step] + sched[step];
-            uint32_t t2 = Sig0(A) + Maj(A, B, C);
-
-            H = G;
-            G = F;
-            F = E;
-            E = D + t1;
-            D = C;
-            C = B;
-            B = A;
-            A = t1 + t2;
+        for (size_t t = 16; t < 64; ++t) {
+            W[t] = sig1(W[t - 2]) + W[t - 7] + sig0(W[t - 15]) + W[t - 16];
         }
 
-        words->A += A;
-        words->B += B;
-        words->C += C;
-        words->D += D;
-        words->E += E;
-        words->F += F;
-        words->G += G;
-        words->H += H;
+        uint32_t a = words->a;
+        uint32_t b = words->b;
+        uint32_t c = words->c;
+        uint32_t d = words->d;
+        uint32_t e = words->e;
+        uint32_t f = words->f;
+        uint32_t g = words->g;
+        uint32_t h = words->h;
+
+        for (size_t t = 0; t < 64; ++t) {
+            uint32_t t1 = h + Sig1(e) + Ch(e, f, g) + K[t] + W[t];
+            uint32_t t2 = Sig0(a) + Maj(a, b, c);
+
+            h = g;
+            g = f;
+            f = e;
+            e = d + t1;
+            d = c;
+            c = b;
+            b = a;
+            a = t1 + t2;
+        }
+
+        words->a += a;
+        words->b += b;
+        words->c += c;
+        words->d += d;
+        words->e += e;
+        words->f += f;
+        words->g += g;
+        words->h += h;
     }
     free(buf.bytes);
     return 0;
