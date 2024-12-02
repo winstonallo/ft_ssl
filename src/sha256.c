fef4a48903e4d356e1dab5d0a19baf6b990d4aa4
winstonallo <arthurbiedchar@gmail.com>
2024-12-02 09:45:41 +0100

    fix: simplify calculate_padding logic

--- src/sha256.c
+++ src/sha256.c
@@ -9,26 +9,26 @@
 
 // First 32 bits of the fractional parts of the square roots of the first eight
 // prime numbers.
-#define DFLT_A 0x6a09e667UL
-#define DFLT_B 0xbb67ae85UL
-#define DFLT_C 0x3c6ef372UL
-#define DFLT_D 0xa54ff53aUL
-#define DFLT_E 0x510e527fUL
-#define DFLT_F 0x9b05688cUL
-#define DFLT_G 0x1f83d9abUL
-#define DFLT_H 0x5be0cd19UL
+#define DFLT_A 0x6a09e667
+#define DFLT_B 0xbb67ae85
+#define DFLT_C 0x3c6ef372
+#define DFLT_D 0xa54ff53a
+#define DFLT_E 0x510e527f
+#define DFLT_F 0x9b05688c
+#define DFLT_G 0x1f83d9ab
+#define DFLT_H 0x5be0cd19
 
 // First 32 bits of the fractional parts of the cube roots of the first 64
 // prime numbers.
 static const uint32_t K[] = {
-    0x428a2f98UL, 0x71374491UL, 0xb5c0fbcfUL, 0xe9b5dba5UL, 0x3956c25bUL, 0x59f111f1UL, 0x923f82a4UL, 0xab1c5ed5UL,
-    0xd807aa98UL, 0x12835b01UL, 0x243185beUL, 0x550c7dc3UL, 0x72be5d74UL, 0x80deb1feUL, 0x9bdc06a7UL, 0xc19bf174UL,
-    0xe49b69c1UL, 0xefbe4786UL, 0x0fc19dc6UL, 0x240ca1ccUL, 0x2de92c6fUL, 0x4a7484aaUL, 0x5cb0a9dcUL, 0x76f988daUL,
-    0x983e5152UL, 0xa831c66dUL, 0xb00327c8UL, 0xbf597fc7UL, 0xc6e00bf3UL, 0xd5a79147UL, 0x06ca6351UL, 0x14292967UL,
-    0x27b70a85UL, 0x2e1b2138UL, 0x4d2c6dfcUL, 0x53380d13UL, 0x650a7354UL, 0x766a0abbUL, 0x81c2c92eUL, 0x92722c85UL,
-    0xa2bfe8a1UL, 0xa81a664bUL, 0xc24b8b70UL, 0xc76c51a3UL, 0xd192e819UL, 0xd6990624UL, 0xf40e3585UL, 0x106aa070UL,
-    0x19a4c116UL, 0x1e376c08UL, 0x2748774cUL, 0x34b0bcb5UL, 0x391c0cb3UL, 0x4ed8aa4aUL, 0x5b9cca4fUL, 0x682e6ff3UL,
-    0x748f82eeUL, 0x78a5636fUL, 0x84c87814UL, 0x8cc70208UL, 0x90befffaUL, 0xa4506cebUL, 0xbef9a3f7UL, 0xc67178f2};
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
 
 typedef struct Words {
     uint32_t a;
@@ -73,11 +73,7 @@ Maj(uint32_t a, uint32_t b, uint32_t c) {
 
 size_t
 calculate_padding(size_t original_size) {
-    if (original_size % 64 > 55) {
-        return SHA256_BLOCK_SIZE - ((original_size % SHA256_BLOCK_SIZE) + 1) + 56;
-    } else {
-        return SHA256_BLOCK_SIZE - ((original_size % SHA256_BLOCK_SIZE) + 1) - 8;
-    }
+    return SHA256_BLOCK_SIZE - ((original_size % SHA256_BLOCK_SIZE) + 1) + ((original_size % 64 > 55) ? 56 : (-8));
 }
 
 Message
