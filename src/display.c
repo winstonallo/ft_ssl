3dc214ff0e7fe16d97f9eff817ac8981701fe4e1
winstonallo <arthurbiedchar@gmail.com>
2024-12-03 16:39:17 +0100

    add: -q and -r options

--- src/display.c
+++ src/display.c
@@ -5,6 +5,11 @@
 
 void
 display(char *hash, char *algo_name, File *file, const struct Options *const opts) {
+    if (opts->q) {
+        ft_printf(STDOUT_FILENO, "%s\n", hash);
+        return;
+    }
+
     ssize_t len = opts->p ? file->content_size : (ssize_t)ft_strlen(file->path);
     char to_print[len];
 
@@ -13,7 +18,12 @@ display(char *hash, char *algo_name, File *file, const struct Options *const opt
     } else {
         ft_memcpy(to_print, file->path, len);
     }
-    to_print[file->content_size - 1] = '\0';
 
-    ft_printf(STDOUT_FILENO, "%s(%s)= %s\n", algo_name, to_print, hash);
+    to_print[len] = '\0';
+
+    if (opts->r) {
+        ft_printf(STDOUT_FILENO, "%s *%s\n", hash, to_print);
+    } else {
+        ft_printf(STDOUT_FILENO, "%s(%s)= %s\n", algo_name, to_print, hash);
+    }
 }
