9e9b6f3e66f4f0bad1b9e833c5cc03756ff4afeb
winstonallo <arthurbiedchar@gmail.com>
2024-12-06 14:03:14 +0100

    update: libft

--- libft/src/bit/bit.c
+++ libft/src/bit/bit.c
@@ -1,7 +1,6 @@
 #include <stdint.h>
 #include <sys/types.h>
 
-// Rotates `val` to the left by `by` bits.
 uint32_t
 rotl_32(uint32_t val, uint32_t by) {
     if ((by &= 32 - 1) == 0) {
@@ -10,7 +9,6 @@ rotl_32(uint32_t val, uint32_t by) {
     return (val << by) | (val >> (32 - by));
 }
 
-// Rotates `val` to the right by `by` bits.
 uint32_t
 rotr_32(uint32_t val, uint32_t by) {
     if ((by &= 32 - 1) == 0) {
